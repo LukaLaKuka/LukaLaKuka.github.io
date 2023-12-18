ee1ce87e8ef9c072f018c3240184d128c9400277
Tomás Nahuel Antela Rizzo <tomasnahuelantela@gmail.com>
2023-12-18 22:52:12 +0000

    Checkpoint

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import "bootstrap/dist/css/bootstrap-grid.min.css";
+  import Layout from "./sections/Layout.svelte";
+  import Presentation from "./sections/Presentation/Presentation.svelte";
+</script>
+
+<Layout>
+  <Presentation/>
+</Layout>
---
+++ src/sections/Layout.svelte
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-2 d-none d-md-flex"></div>
+        <div class="col">
+            <slot />
+        </div>
+        <div class="col-md-2 d-none d-md-flex"></div>
+    </div>
+</div>
