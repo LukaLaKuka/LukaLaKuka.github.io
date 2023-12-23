914702a6ca737a0baa9c9f218a78c356d92c6640
Tomhuel <tomasnahuelantela@gmail.com>
2023-12-23 23:53:53 +0000

    Astro migration + Presentation Module

--- src/App.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import "bootstrap/dist/css/bootstrap-grid.min.css";
-  import Layout from "./sections/Layout.svelte";
-  import Presentation from "./sections/Presentation/Presentation.svelte";
-</script>
-
-<Layout>
-  <Presentation/>
-</Layout>
--- src/sections/Layout.svelte
+++
@@ -1,9 +0,0 @@
-<div class="container">
-    <div class="row">
-        <div class="col-md-2 d-none d-md-flex"></div>
-        <div class="col">
-            <slot />
-        </div>
-        <div class="col-md-2 d-none d-md-flex"></div>
-    </div>
-</div>
