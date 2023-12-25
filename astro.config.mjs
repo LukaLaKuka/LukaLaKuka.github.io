b799104a8982c911925777abd8b9a62b3b1cf808
Tomhuel <tomasnahuelantela@gmail.com>
2023-12-25 23:47:33 +0000

    Livewire added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+const LIVE_URL = "https://tomhuel.github.io";
+
 // https://astro.build/config
 export default defineConfig({
+  site: LIVE_URL,
   integrations: [tailwind()]
 });
