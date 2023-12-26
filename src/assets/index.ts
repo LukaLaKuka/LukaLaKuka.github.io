629ca611c5327fa1df069517f835477fc627dff6
Tomhuel <tomasnahuelantela@gmail.com>
2023-12-26 22:33:28 +0000

    Portfolio Done :D

---
+++ src/assets/index.ts
@@ -0,0 +1,59 @@
+import discordSvg from "./others/discord.svg";
+import linkSvg from "./link.svg";
+import bootstrapSvg from "./technologies/bootstrap.svg";
+import composerSvg from "./technologies/composer.svg";
+import cssSvg from "./technologies/css.svg";
+import debianSvg from "./technologies/debian.svg";
+import gitSvg from "./technologies/git.svg"
+import githubSvg from "./technologies/github.svg"
+import htmlSvg from "./technologies/html.svg"
+import javascriptSvg from "./technologies/javascript.svg"
+import jestSvg from "./technologies/jest.svg"
+import jsonSvg from "./technologies/json.svg"
+import laravelSvg from "./technologies/laravel.svg"
+import livewireSvg from "./technologies/livewire.svg"
+import mysqlSvg from "./technologies/mysql.svg"
+import nextSvg from "./technologies/next.svg"
+import nodeSvg from "./technologies/node.svg"
+import npmSvg from "./technologies/npm.svg"
+import phpSvg from "./technologies/php.svg"
+import reactSvg from "./technologies/react.svg"
+import tailwindSvg from "./technologies/tailwind.svg"
+import typescriptSvg from "./technologies/typescript.svg"
+import xmlSvg from "./technologies/xml.svg"
+import adaptabilitySvg from "./softSkills/adaptability.svg"
+import analysisSvg from "./softSkills/analysis.svg"
+import communicationSvg from "./softSkills/communication.svg"
+import initiativeSvg from "./softSkills/initiative.svg"
+import teamWorkSvg from "./softSkills/teamWork.svg"
+
+export {
+    discordSvg,
+    linkSvg,
+    bootstrapSvg,
+    composerSvg,
+    cssSvg, 
+    debianSvg, 
+    gitSvg, 
+    githubSvg, 
+    htmlSvg, 
+    javascriptSvg, 
+    jestSvg, 
+    jsonSvg, 
+    laravelSvg, 
+    livewireSvg, 
+    mysqlSvg, 
+    nextSvg, 
+    nodeSvg, 
+    npmSvg, 
+    phpSvg, 
+    reactSvg, 
+    tailwindSvg, 
+    teamWorkSvg, 
+    typescriptSvg, 
+    xmlSvg, 
+    adaptabilitySvg, 
+    analysisSvg, 
+    communicationSvg, 
+    initiativeSvg
+}
