8aa8229a2b1f2063eea6d523f7853bd1b9d8ac90
Tomás Nahuel Antela Rizzo <tomasnahuelantela@gmail.com>
2024-01-21 23:11:35 +0000

    Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,21 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 const LIVE_URL = "https://tomhuel.github.io";
+import partytown from "@astrojs/partytown"
 
 
 // https://astro.build/config
 export default defineConfig({
   site: LIVE_URL,
-  integrations: [tailwind(), svelte()]
+  integrations: [
+    svelte(),
+    tailwind(),
+    mdx(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
