909457a949bd2ced668f7b3e595751591b37f676
Tomhuel <tomasnahuelantela@gmail.com>
2024-05-03 23:08:19 +0100

    Surprise :)

--- src/modules/Contact/Contact.svelte
+++ src/modules/Contact/Contact.svelte
@@ -12,6 +12,11 @@
             showTooltip = false;
         }, 2000);
     }
+
+    function funnyHello() {
+        window.location.href =
+            "https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1&mute=1";
+    }
 </script>
 
 <section class="mb-20 mt-5 flex flex-col gap-5 w-full">
@@ -24,7 +29,7 @@
             id="email"
             class="p-2 rounded-lg bg-slate-200 w-full md:w-1/2"
         />
-        <div class="flex w-full md:w-auto justify-center items-center gap-5 ">
+        <div class="flex w-full md:w-auto justify-center items-center gap-5">
             <button
                 on:click={copyToClipboard}
                 class="button bg-gray-950 bg-opacity-70"
@@ -63,6 +68,23 @@
                     />
                 </svg>
             </a>
+            <button
+                on:click={funnyHello}
+                class="button bg-gray-950 bg-opacity-70"
+                title="Favoritos"
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    fill="currentColor"
+                    class="bi bi-star-fill"
+                    viewBox="0 0 16 16"
+                >
+                    <path
+                        d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"
+                    />
+                </svg>
+            </button>
         </div>
     </div>
     <div class="flex justify-around">
